6048ade9a894ee014e8171d06cb072edbce1e6b7
Adong-o <adongo969@gmail.com>
2023-05-18 08:53:10 +0300

    Personal Finance

---
+++ index.js
@@ -0,0 +1,103 @@
+function showMoreInfo() {
+    var info = document.getElementById("more-info");
+    var button = document.getElementById("show-more-button");
+
+    if (info.style.display === "none") {
+      info.style.display = "block";
+      button.innerHTML = "Hide Info";
+    } else {
+      info.style.display = "none";
+      button.innerHTML = "Show Info";
+    }
+  }
+
+  function showMoreInfo2() {
+    var info = document.getElementById("more-info-2");
+    var button = document.getElementById("show-more-button-2");
+
+    if (info.style.display === "none") {
+      info.style.display = "block";
+      button.innerHTML = "Hide Info";
+    } else {
+      info.style.display = "none";
+      button.innerHTML = "Show Info";
+    }
+  }
+
+  function showMoreInfo3() {
+    var info = document.getElementById("more-info-3");
+    var button = document.getElementById("show-more-button-3");
+
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        button.innerHTML = "Hide Info"
+    } else {
+      info.style.display = "none";
+      button.innerHTML = "Show Info";
+    }
+  }
+
+  function showMoreInfo4() {
+    var info = document.getElementById("more-info-4");
+    var button = document.getElementById("show-more-button-4");
+
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        button.innerHTML = "Hide Info"
+    } else {
+      info.style.display = "none";
+      button.innerHTML = "Show Info";
+    }
+  }
+
+  function showMoreInfo5() {
+    var info = document.getElementById("more-info-5");
+    var button = document.getElementById("show-more-button-5");
+
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        button.innerHTML = "Hide Info"
+    } else {
+      info.style.display = "none";
+      button.innerHTML = "Show Info";
+    }
+  }
+
+  function showMoreInfo6() {
+    var info = document.getElementById("more-info-6");
+    var button = document.getElementById("show-more-button-6");
+
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        button.innerHTML = "Hide Info"
+    } else {
+      info.style.display = "none";
+      button.innerHTML = "Show Info";
+    }
+  }
+
+  function showMoreInfo7() {
+    var info = document.getElementById("more-info-7");
+    var button = document.getElementById("show-more-button-7");
+
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        button.innerHTML = "Hide Info"
+    } else {
+      info.style.display = "none";
+      button.innerHTML = "Show Info";
+    }
+  }
+
+  function showMoreInfo8() {
+    var info = document.getElementById("more-info-8");
+    var button = document.getElementById("show-more-button-8");
+
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        button.innerHTML = "Hide Info"
+    } else {
+      info.style.display = "none";
+      button.innerHTML = "Show Info";
+    }
+  }
